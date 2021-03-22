0c477395533d905b4fdd4e9f152569877ba2a88b
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-03-22 13:57:20 -0300

    challenge finished

---
+++ src/@types/Food.d.ts
@@ -0,0 +1,8 @@
+interface IFood {
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+}
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,21 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
+import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: IInputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
